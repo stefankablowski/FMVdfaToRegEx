3e3013622f2940960b03cdd5ff20150fde4b46e7
stefankablowski <49285379+stefankablowski@users.noreply.github.com>
2019-11-30 12:10:16 +0100

    build html and working parse

--- resources/javascript/start.js
+++ resources/javascript/start.js
@@ -20,5 +20,38 @@ let dfa = {
 }
 
 window.onload = ()=>{
-        dfaToRegEx(dfa);
+    let example1 = 
+    `{
+        initial: "1",
+        states: {
+            1: {
+                on: {
+                    a: "2",
+                    b: "3"
+                }
+            },
+            2: {
+                on: {
+                    a: "2",
+                    b: "3"
+                }
+            },
+            3: {
+            }
+        },
+        final:[ "3" ]
+    }`
+    document.getElementById('inputDFA').value = example1;
 }
+
+let startAlgorithm = ()=>{
+    //Do some complicated parsing stuff i dont understand but it just works
+    //Note here: never use ":" inside values
+    let badJSONdfa = String(document.getElementById('inputDFA').value).replace(/(\r\n|\n|\r)/gm, "");
+    goodJSONdfa = badJSONdfa.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
+    parsedDFA = JSON.parse(goodJSONdfa);
+    dfaToRegEx(parsedDFA);
+}
+
+document.getElementById('startAlgorithm').addEventListener('click',startAlgorithm);
+
